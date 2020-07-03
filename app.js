b5387c8338ee2b3bba46f45004e77e517a2c376d
MURALIDHR-code <55774808+MURALIDHR-code@users.noreply.github.com>
2020-07-03 19:30:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 'use strict';
 
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
-var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
-// const AssistantV1 = require('ibm-watson/assistant/v1');
+const AssistantV1 = require('ibm-watson/assistant/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
 var app = express();
@@ -35,29 +35,27 @@ console.log("assistantVersion = " + assistantVersion);
 
 // Create the service wrapper
 const assistant = new AssistantV1({
-    version: assistantVersion,
-    authenticator: new IamAuthenticator({
-        apikey: assistantAPIKey,
-    }),
-    url: assistantURL,
-  });
+  version: assistantVersion,
+  authenticator: new IamAuthenticator({
+    apikey: assistantAPIKey,
+  }),
+  url: assistantURL,
+});
 
 
 // Endpoint to be call from the client side
-app.post('/api/message', function(req, res){
-// });
-    console.log("");
-    var workspace = getDestinationBot(req.body.context) || 'cd63209f-5fd3-4e12-99ed-455d1452ffc1';
-    console.log("workspace = " + workspace);
-	});
-    if (!workspace || workspace === 'cd63209f-5fd3-4e12-99ed-455d1452ffc1') {
-        return res.json({
-            'output': {
-                'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-            }
-        });
-    }
-    var payload = {
+app.post('/api/message', function (req, res) {
+  console.log("");
+  var workspace = getDestinationBot(req.body.context) || 'cd63209f-5fd3-4e12-99ed-455d1452ffc1';
+  console.log("workspace = " + workspace);
+  if (!workspace || workspace === 'cd63209f-5fd3-4e12-99ed-455d1452ffc1') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var payload = {
     workspaceId: workspace,
     context: req.body.context || {},
     input: req.body.input || {}
@@ -90,10 +88,10 @@ app.post('/api/message', function(req, res){
       });
     } else { // There is no redirect. So send back the response to user for further action
       return res.json(updateMessage(payload, data));
-    });
+    }
 
   });
-
+});
 
 // The function checks if the bot response says messages to be redirected
 function isRedirect(context) {
@@ -112,64 +110,55 @@ function isRedirect(context) {
 // The agent bot decides which bot the request should be redirected to and updates that in context variable.
 // Get worspace_id for redirected bot details so messages can be sent to that bot
 function getDestinationBot(context) {
-    var destination_bot = null;
-    if (context && context.destination_bot) {
-        destination_bot = context.destination_bot.toUpperCase();
-    }
-    
-    var wsId = process.env["WORKSPACE_ID_" + destination_bot];
-    
-    if (!wsId) {
-        wsId = process.env["WORKSPACE_ID_agentBot"];
-    }
-    
-    if (!destination_bot) {
-        destination_bot = "agentBot";
-    }
-    
-    console.log("Message being sent to: " + destination_bot + " bot");
-    return wsId;
-    
-    /**
-     * Updates the response text using the intent confidence
-     * @param  {Object} input The request to the Assistant service
-     * @param  {Object} response The response from the Assistant service
-     * @return {Object}          The response with the updated message
-     */
-    if (context && context.destination_bot && context.destination_bot.toUpperCase() === "ResiliencySkills") {
-        return process.env['02d797e3-f75b-4403-a229-18346e4869de'];
-    } else if (context && context.destination_bot && context.destination_bot.toUpperCase() === "BluepagesSkills") {
-        return process.env['ca39e110-7537-4f19-841c-bb700c780817'];
+  var destination_bot = null;
+  if (context && context.destination_bot) {
+    destination_bot = context.destination_bot.toUpperCase();
+  }
+
+  var wsId = process.env["WORKSPACE_ID_" + destination_bot];
+
+  if (!wsId) {
+    wsId = process.env["WORKSPACE_ID_agentBot"];
+  }
+
+  if (!destination_bot) {
+    destination_bot = "agentBot";
+  }
+
+  console.log("Message being sent to: " + destination_bot + " bot");
+  return wsId;
+}
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    console.log("Response message: " + JSON.stringify(response.output.text));
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
     } else {
-        return process.env["WORKSPACE_ID_agentBot"];
+      responseText = 'I did not understand your intent';
     }
-    
-    function updateMessage(input, response) {
-        var responseText = null;
-        if (!response.output) {
-            response.output = {};
-        } else {
-            console.log("Response message: " + JSON.stringify(response.output.text));
-            return response;
-        }
-        if (response.intents && response.intents[0]) {
-            var intent = response.intents[0];
-            // Depending on the confidence of the response the app can return different messages.
-            // The confidence will vary depending on how well the system is trained. The service will always try to assign
-            // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-            // user's intent . In these cases it is usually best to return a disambiguation message
-            // ('I did not understand your intent, please rephrase your question', etc..)
-            if (intent.confidence >= 0.75) {
-                responseText = 'I understood your intent was ' + intent.intent;
-            } else if (intent.confidence >= 0.5) {
-                responseText = 'I think your intent was ' + intent.intent;
-            } else {
-                responseText = 'I did not understand your intent';
-            }
-        }
-        response.output.text = responseText;
-        return response;
-    }
-    
-    
-    module.exports = app;
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+module.exports = app;
