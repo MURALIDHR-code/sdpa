bceed8402f1e72896ccadf64fc466618ca05d74a
MURALIDHR-code <55774808+MURALIDHR-code@users.noreply.github.com>
2020-07-10 21:42:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -47,13 +47,13 @@
 		// Endpoint to be call from the client side
 		app.post('/api/message', function (req, res) {
 		  console.log("");
-		  // var destination_bot = '5758ad9a-c37c-4894-abf7-9368cbdba4c0 - 6abc5c45-fcdc-42ab-9f39-1d0baf31f09f - 2e1ab090-5f51-465e-97ad-c831ec32898c';
-		  var workspace = getDestinationBot(req.body.context) || 'destination_bot';
+		  // workspace = '5758ad9a-c37c-4894-abf7-9368cbdba4c0 - 6abc5c45-fcdc-42ab-9f39-1d0baf31f09f - 2e1ab090-5f51-465e-97ad-c831ec32898c';
+		  var workspace = getDestinationBot(req.body.context) || '<workspace-id>';
 		  console.log("workspace = " + workspace);
-		  if (!workspace || workspace === 'destination_bot') {
+		  if (!workspace || workspace === '<workspace-id>') {
 		    return res.json({
 				'output': {
-     		       'text': text 
+     		       'text': 'text'
 		      }
 		    });
 		  }
